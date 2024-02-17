423c1538784e1e6e224f23af8c0b55d6510a2bbb
Chaido Poulianou <poulianouxaido@gmail.com>
2024-02-18 01:06:36 +0200

    graphs

--- agents_fictitious_play.py
+++ agents_fictitious_play.py
@@ -9,7 +9,7 @@
 # keep track of each agents empirical frequency of strategies played
 
 # GAMES:
-# 1. Matching Pennies:  pure competitive, mixed strategies, zero-sum ( + stochastic version with coin toss)
+# 1. Matching Pennies:  pure competitive, 1 mixed Nash, zero-sum ( + stochastic version with coin toss)
 # 2. Prisoner's Dillema: zero-sum, 1 pure Nash
 # 3. Battle-of-sexes: mixed cooperative-competitive, 2 pure Nash, 1 mixed
 # 4. (Left-Right): pure cooperative, 2 pure Nash, 1 mixed
@@ -38,7 +38,10 @@ def compute_expect_utility(actions, belief, payoff_matrix):
     exp_ut = []
     for action in range(len(actions)):
         # (action, H), (action,T)
-        exp_ut.append(payoff_matrix[action][0]*belief[0] + payoff_matrix[action][1]*belief[1])
+        sum = 0
+        for opponnent_action in range(len(actions)):
+            sum += payoff_matrix[action][opponnent_action]*belief[opponnent_action]
+        exp_ut.append(sum)
     return best_response(actions, exp_ut)
     
 def update_beliefs(actions, action_count):
@@ -75,12 +78,14 @@ INITIALIZATION OF GAMES
         -> extra : Rock-paper-Scissor: general-sum
 """
 def matching_pennies_init(advanced=False):
-    actions = {0: "H", 1: "T"}
+    
     # Advanced mode is Rock-Papers-Scissors
     if advanced:
+        actions = {0: "R", 1: "P", 2: "S"}
         payoff_matrix_agent1 = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0]])
         payoff_matrix_agent2 = np.array([[0, 1, -1], [-1, 0, 1], [-1, 1, 0]])
-    else:   
+    else:
+        actions = {0: "H", 1: "T"}
         payoff_matrix_agent1 = np.array([[1, -1], [-1, 1]])
         payoff_matrix_agent2 = np.array([[-1, 1], [1, -1]])
     payoff_matrices = [payoff_matrix_agent1, payoff_matrix_agent2]
@@ -90,16 +95,36 @@ def matching_pennies_init(advanced=False):
         action_count = [[0, 1, 0], [0, 1, 0]]
     else:
         action_count = [[0,1], [0,1]]
-
+    #action_count = [[1.5, 2], [2, 1.5]]
+    
     # initialise beliefs with mixed strategy of each agents
     # each row is the mixed strategy/ probability of all actions of the agent
     # empirical_distribution
     # P(a) = w(a)/SUM(w(a')), probability of opponent playing action a and a' all actions (includiing a)
-    beliefs = np.array(([action_count[1][0]/sum(action_count[1]), action_count[1][1]/sum(action_count[1])], [action_count[0][0]/sum(action_count[0]), action_count[0][1]/sum(action_count[0])]))
-    #beliefs = np.array(([1.5, 2], [2, 1.5]))
+    belief1 = []
+    belief2  =[]
+    for action in range(len(actions)):
+        belief1.append(action_count[1][action]/sum(action_count[1]))
+        belief2.append(action_count[0][action]/sum(action_count[0]))
+    beliefs = np.array((belief1, belief2))
+    print(beliefs)
+    
 
     return actions, payoff_matrices, action_count, beliefs
 
+'''
+#################################
+#    2. Prisoners Dillema       # 
+#################################
+            S         B
+        ###################
+    S   # (-1, -1)  (-3, 0)#
+    B   # (0, -3)  (-2, -2)#
+        ###################
+
+        1 pure Nash: 
+            Betray-Betray
+'''
 def prisoners_dillema_init():
     actions = {0: "Silent", 1: "Betray"}
     payoff_matrix_agent1 = np.array([[-1, -3], [0, -2]])
@@ -107,30 +132,61 @@ def prisoners_dillema_init():
     payoff_matrices = [payoff_matrix_agent1, payoff_matrix_agent2]
     # count of H and T played by each agent
     # for state 0 lets assume both have played Tails
-    action_count = [[0,1], [0,1]]
+    action_count = [[1,0], [1,0]]
 
     
     beliefs = np.array(([action_count[1][0]/sum(action_count[1]), action_count[1][1]/sum(action_count[1])], [action_count[0][0]/sum(action_count[0]), action_count[0][1]/sum(action_count[0])]))
 
     return actions, payoff_matrices, action_count, beliefs
 
+'''
+#################################
+#    3. Battle of Sexes        # 
+#################################
+            O         M
+        ###################
+    O   # (3, 2)  (0, 0)#
+    M   # (0, 0)  (2, 3)#
+        ###################
+
+        2 pure Nash: 
+            -> Opera-Opera
+            -> Movies-Movies
+        1 mixed Nash:
+            -> Opera (3/5) - Opera (2/5)
+'''
 def battle_of_sexes_init():
     # actions
-    actions = {0: "Opera", 1: "Football"}
+    actions = {0: "Opera", 1: "Movies"}
     # reward
-    payoff_matrix_agent1 = np.array([[3, 2], [0, 2]])
+    payoff_matrix_agent1 = np.array([[3, 0], [0, 2]])
     payoff_matrix_agent2 = np.array([[2, 0], [0, 3]])
     payoff_matrices = [payoff_matrix_agent1, payoff_matrix_agent2]
     # count of H and T played by each agent
     # for state 0 lets assume both have played Tails
-    action_count = [[0,1], [0,1]]
+    action_count = [[0,1], [1,0]]
 
     beliefs = np.array(([action_count[1][0]/sum(action_count[1]), action_count[1][1]/sum(action_count[1])], [action_count[0][0]/sum(action_count[0]), action_count[0][1]/sum(action_count[0])]))
 
     return actions, payoff_matrices, action_count, beliefs
 
-def main(game):
-    print(game)
+def left_right():
+    # actions
+    actions = {0: "L", 1: "R"}
+    # reward
+    payoff_matrix_agent1 = np.array([[1 , 0], [0, 1]])
+    payoff_matrix_agent2 = np.array([[1 , 0], [0, 1]])
+    payoff_matrices = [payoff_matrix_agent1, payoff_matrix_agent2]
+    # count of H and T played by each agent
+    # for state 0 lets assume both have played Tails
+    action_count = [[1,0], [0.5, 0.5]]
+
+    beliefs = np.array(([action_count[1][0]/sum(action_count[1]), action_count[1][1]/sum(action_count[1])], [action_count[0][0]/sum(action_count[0]), action_count[0][1]/sum(action_count[0])]))
+
+    return actions, payoff_matrices, action_count, beliefs
+
+def main(game, advanced):
+    print(game, advanced)
     game = int(game)
     if game == 2:
         actions, payoff_matrices, action_count, beliefs = prisoners_dillema_init()
@@ -138,8 +194,11 @@ def main(game):
     elif game == 3:
         actions, payoff_matrices, action_count, beliefs = battle_of_sexes_init()
         print('PLAYING BATTLE OF SEXES')
+    elif game == 4:
+        actions, payoff_matrices, action_count, beliefs = left_right()
+        print('PLAYING LEFT-RIGHT')
     else:
-        actions, payoff_matrices, action_count, beliefs = matching_pennies_init()
+        actions, payoff_matrices, action_count, beliefs = matching_pennies_init(advanced)
         print('PLAYING MATCHING PENNIES')
 
 
@@ -185,15 +244,28 @@ def main(game):
             subplot.plot(list(range(0,len(probs))), action_probs, linewidth=1.5, label=f"s{action} = {actions[action]}", color=colors[action])
         subplot.legend()
 
-        subplot.set_ylim(0, 1)
+    plt.tight_layout()
     plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--game", help="choose which game to play. Range [1-3]")
+    parser.add_argument("--adv", help="If game 1 chosen, seet this to 2 for rock-papers-scissors. Default macthing pennies")
     args = parser.parse_args()
     game = args.game
+    advanced = args.adv
     # check if game option given, default matching pennies
+    print(game, advanced)
     if game is None:
         game = 1
-    main(game)
+    if advanced is not None:
+        print('ok')
+        advanced = int(advanced)
+        if advanced == 2:
+            print('yy')
+            advanced = True
+        else:
+            advanced = False
+    else:
+        advanced  = False
+    main(game, advanced)
